69fb789e433e07f20d3d7b46efe8a50f6b9778bf
Szymon Berdzik <szymon19314@wp.pl>
2022-03-08 23:40:22 +0100

    adding auth tests

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const {User} = require("../../models/user");
 const {Genre} = require("../../models/genre");
 const server = require('../../app');
+const bcrypt = require("bcrypt");
 describe('auth middleware', () => {
 
 
@@ -34,4 +35,81 @@ describe('auth middleware', () => {
 
 describe('/api/auth', () => {
 
-})
+    describe('/api/logout', () => {
+        it('should delete auth header', async () => {
+            const res = await request(server).delete('/api/auth/logout');
+
+            expect(res.header).not.toHaveProperty('x-auth-token', '');
+        });
+
+        it('should return true in response', async () => {
+            const res = await request(server).delete('/api/auth/logout');
+
+            expect(res.body).toBe(true);
+        });
+    });
+
+    describe('POST /', () => {
+
+        let email;
+        let password;
+        let user;
+        let salt;
+        beforeAll(async () => {
+            salt = await bcrypt.genSalt(10);
+        });
+
+        beforeEach(async () => {
+            email = 'test@test.com';
+            password = 'Test1234@';
+            user = await User.create({
+                email: email,
+                password: await bcrypt.hash(password, salt),
+                name: 'Test'
+            });
+        });
+        afterEach(async () => {
+            await User.deleteMany({});
+        });
+        const exec = () => {
+            return request(server)
+                .post('/api/auth')
+                .send({email, password});
+        };
+        it('should return 400 if user is not registered', async () => {
+            await User.deleteMany({});
+            const res = await exec();
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if password verify fail', async () => {
+            password = 'TestTest@123';
+            const res = await exec();
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if email is invalid', async () => {
+            email = 'test';
+            const res = await exec();
+
+            expect(res.status).toBe(400);
+        });
+
+        it('should return 400 if password is invalid', async () => {
+            password = 'test';
+            const res = await exec();
+
+            expect(res.status).toBe(400);
+        });
+
+
+        it('should set auth token in header', async () => {
+            const res = await exec();
+            console.log(res.error);
+
+            expect(res.header).toHaveProperty('x-auth-token', user.generateAuthToken());
+        });
+    });
+});
