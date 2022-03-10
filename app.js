f13ff1f5f420be966414430d26e12603864176a4
Szymon Berdzik <szymon19314@wp.pl>
2022-03-10 23:56:39 +0100

    preparing api for production

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 if(process.env.NODE_ENV !== 'test') {
     app.listen(port, () => logger.info(`Listening on port ${port}`));
 }
