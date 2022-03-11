985043c323539e4a2b5bc5ffcfeb664f3b61967b
Szymon Berdzik <szymon19314@wp.pl>
2022-03-11 12:41:59 +0100

    .

--- controllers/rentals.js
+++ controllers/rentals.js
@@ -23,6 +23,7 @@ exports.create = async (req, res) => {
     if (movie.numberInStock === 0)
         return res.status(400).send('Movie is not in the stock');
 
+
     let rental = new Rental({
         customer: {
             _id: customer._id,
@@ -40,13 +41,15 @@ exports.create = async (req, res) => {
 
     // transaction
     try {
-        await new Fawn.Task()
-            .save('rentals', rental)
-            .update('movies', {_id: movie._id}, {
-                $inc: {numberInStock: -1}
-            })
-            .run();
-        res.send(rental);
+        await rental.save();
+
+        // await new Fawn.Task()
+        //     .save('rentals', rental)
+        //     .update('movies', {_id: movie._id}, {
+        //         $inc: {numberInStock: -1}
+        //     })
+        //     .run();
+        // res.send(rental);
     } catch (ex) {
         res.status(500).send("Something failed");
     }
