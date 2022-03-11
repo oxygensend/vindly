e1cecfd9295f8bf65f2e892b837c296dc246bf90
Szymon Berdzik <szymon19314@wp.pl>
2022-03-11 11:46:22 +0100

    .

--- logger.js
+++ logger.js
@@ -2,12 +2,13 @@ const winston = require("winston");
 const config = require("config")
 require('winston-mongodb');
 
+const uri = process.env.MONGODB_URI || config.get('db');
 module.exports = winston.createLogger({
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({filename: 'logfile.log'}),
         new winston.transports.MongoDB({
-            db: config.get('db'),
+            db: uri,
             options: {useUnifiedTopology: true}
         }),
     ]
