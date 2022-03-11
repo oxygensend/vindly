9f6f1b92e2a2a64621e99bf8bb4491d10f5d0dbc
Szymon Berdzik <szymon19314@wp.pl>
2022-03-11 13:04:07 +0100

    fixing rentals POST bug

--- startup/db.js
+++ startup/db.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 const logger = require('../logger');
-const config = require('config')
+const config = require('config');
 const Fawn = require("fawn");
 
 const uri = process.env.MONGODB_URI || config.get('db');
 console.log(uri);
 module.exports = () => {
     mongoose.connect(uri)
-        .then(logger.info('Connected to MongoDb'))
-        .catch(e=> logger.error('Cannot connect to mongoDB'));
+        .then(() => {
+                logger.info('Connected to MongoDb');
+                Fawn.init(mongoose);
+            }
+        )
+        .catch(e => logger.error('Cannot connect to mongoDB'));
 
 
-    Fawn.init( mongoose);
 };
