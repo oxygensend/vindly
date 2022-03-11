d898597f5dab97c20557e6e13335202d8163c6c0
Szymon Berdzik <szymon19314@wp.pl>
2022-03-11 10:54:45 +0100

    connecting server with moongoDB

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const logger = require('../logger');
 const config = require('config')
+
+const uri = process.env.MONGODB_URI || config.get('db');
 module.exports = () => {
-    mongoose.connect(config.get('db') )
+    mongoose.connect(uri)
         .then(logger.info('Connected to MongoDb'))
         .catch(e=> logger.error('Cannot connect to mongoDB'));
 };
